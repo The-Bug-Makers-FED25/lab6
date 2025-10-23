3f103416003776f69cff7b1b4f44020250894620
Cailean Matthews <matthewsmcailean@gmail.com>
2025-10-23 13:57:31 -0400

    Fixed spelling

--- blog-app/src/components/BlogBox/BlogBox.jsx
+++ blog-app/src/components/BlogBox/BlogBox.jsx
@@ -5,7 +5,7 @@ export function BlogBox ({title, content, author, date }) {
             <div className="flex flex-col gap-1.5">
                 <h1 className="text-pdark text-[2.5rem] font-bold">{title}</h1>
                 <p>{content}</p>
-                <p className="font-bold">Auhtor: {author}</p>
+                <p className="font-bold">Author: {author}</p>
                 <p className="font-bold">Date: {date}</p>
             </div>
             <h2 className="text-[1.25rem]">Comments:</h2>
