5f0cb9560987ec8ab6c2b3b4de6eaccae55bf510
Cailean Matthews <matthewsmcailean@gmail.com>
2025-10-28 14:48:14 -0400

    No clue

--- blog-app/src/App.jsx
+++ blog-app/src/App.jsx
@@ -8,7 +8,7 @@ function App() {
   return (
     <>
     <Header />
-    <div className="w-full h-full p-4 bg-grey flex flex-row">
+    <div className="w-full h-[85vh] p-4 bg-grey flex justify-center flex-row">
       <BlogBox title={"My First Blog!"} content={"I uh... I uh haven't done this before..."} author={"Joshua C. Price"} date={"10-22-2025"} initialComments={["He's pretty freaking goated", "This guy isn't human..."]}></BlogBox>
       <BlogBox title={"My Second Blog!"} content={"Don't listen to Josh... He has done this before..."} author={"Cailean M. Matthews."} date={"10-23-2025"} initialComments={["He's the smartest guy i've ever met", "Don't mess with him... his eyes turn red when he's mad..."]}></BlogBox>
     </div>
--- blog-app/src/components/BlogBox/BlogBox.jsx
+++ blog-app/src/components/BlogBox/BlogBox.jsx
@@ -1,7 +1,7 @@
 import BlogComments from "../BlogComments/BlogComments";
 
 export function BlogBox ({title, content, author, date, initialComments }) {
-    return <div className="m-auto p-4 w-fit min-w-[45ch] max-w-[80ch] flex flex-col gap-1 bg-white rounded-md drop-shadow-2xl">
+    return <div className="m-auto p-8 w-fit min-w-[45ch] max-w-[80ch] flex flex-col gap-1 bg-white rounded-md drop-shadow-2xl">
             <div className="flex flex-col gap-1.5">
                 <h1 className="text-pdark text-[2.5rem] font-bold">{title}</h1>
                 <p>{content}</p>
@@ -10,5 +10,5 @@ export function BlogBox ({title, content, author, date, initialComments }) {
             </div>
             <h2 className="text-[1.25rem]">Comments:</h2>
             <BlogComments initialComments={initialComments}/>
-        </ div>
+        </div>
 }
