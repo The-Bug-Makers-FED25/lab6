c95b69af246f97192b8f06cb1efbb8e3d0fceda8
Cailean Matthews <matthewsmcailean@gmail.com>
2025-11-20 14:17:01 -0500

    Fixed homepage size

--- blog-app/src/pages/HomePage.jsx
+++ blog-app/src/pages/HomePage.jsx
@@ -7,11 +7,11 @@ export default function HomePage() {
   const { light } = useToggle();
 
   return (
-    <div className={`min-h-[150vh] flex flex-col items-center pt-20 gap-20 px-4
+    <div className={`min-h-fit flex flex-col items-center px-4
                      ${light ? "bg-grey text-black" : "bg-darkbg text-white"}`}>
 
-        <div className={"flex flex-row items-center justify-center gap-40"}>
-            <div className="flex flex-col justify-center">
+        <div className={"flex flex-row items-center h-[79vh] justify-center gap-40"}>
+            <div className="flex flex-col justify-center gap-4">
                 <h1 className="text-4xl font-bold mb-4 w-80 text-center tracking-wide hover:scale-105 transition-all">
                 Welcome to Our <p className={`hover:scale-120  transition-all hover:tracking-normal  ${light ? "hover:text-pdark" : "hover:text-yellow-300"}`}>Blog App!</p></h1>
                 <div className="flex gap-4 w-full justify-center">
@@ -36,7 +36,7 @@ export default function HomePage() {
             <img src="/lovely-man.jpg" className={"h-[225px] hover:scale-105 transition-all"} alt=""/>
         </div>
 
-        <div className={"flex flex-row items-center justify-center pt-20 gap-40"}>
+        <div className={"flex flex-row h-[90vh] items-center justify-center bg-darkg w-[101.9%] gap-40"}>
             <img src="/monkey.jpg" className={"h-[225px] hover:scale-105 transition-all"} alt=""/>
             <p className="text-lg mb-8 w-100 text-center max-w-xl hover:scale-105 transition-all">
                 <h1 className={`text-4xl font-bold pb-8 ${light ? "hover:text-pdark" : "hover:text-primary"} hover:scale-115 transition-all`}>Who are we?</h1>
@@ -45,7 +45,7 @@ export default function HomePage() {
             </p>
         </div>
 
-        <div className={"flex flex-row items-center justify-center pt-20 gap-40 mb-8"}>
+        <div className={"flex flex-row items-center justify-center h-[90vh] gap-40 mb-8"}>
             <div className={"flex flex-col hover:scale-110 transition-all"}>
                 <Link className={`text-4xl font-bold mb-4 w-100 text-center tracking-wide hover:scale-120 transition-all hover:underline hover:tracking-tight ${light ? "hover:text-pdark" : "hover:text-primary"}`} to="/contact">Contact Us!</Link>
                 <p className={"w-100 text-center"}>We'd love a message from you, be it your thoughts of what to change, or a bug you've discovered.</p>
