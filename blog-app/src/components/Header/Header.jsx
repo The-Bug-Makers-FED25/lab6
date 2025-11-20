8be261bbac076d6d895ecd7360fac49689ec785a
Cailean Matthews <matthewsmcailean@gmail.com>
2025-11-20 12:35:41 -0500

    Toggle theme is not svgs

--- blog-app/src/components/Header/Header.jsx
+++ blog-app/src/components/Header/Header.jsx
@@ -16,10 +16,13 @@ export default function Header() {
             <img className="w-[150px]" src={logo} alt="bug makers logo" />
         </div>
         <nav className="flex gap-2">
+            {light ? <img src="/sun-2-svgrepo-com.svg" alt="SVG of Sun" onClick={toggle} height={"40px"} width={"40px"}/> :
+                <img src="/cloudy-moon-svgrepo-com.svg" alt="SVG of Moon" onClick={toggle} height={"35px"} width={"35px"}/>}
             <Link className="m-1 hover:text-primary" to="/">Home</Link>
             {username ? <Link className="m-1 hover:text-primary" to="/posts">Blogs</Link> : <></>}
             <Link className="m-1 hover:text-primary" to="/contact">Contact</Link>
-            <button className="m-1 hover:text-primary cursor-pointer border-none" onClick={toggle}>Toggle Theme</button>
+
+
             {username ? <button className="m-1 hover:text-primary cursor-pointer border-none" onClick={logout}>Logout</button> : <Link className="m-1 hover:text-primary" to="/login">Login</Link>}
         </nav>
     </header>
